39b0a956f47c317abf64dc3e248b79f7d01398eb
patrickthompsonreis <patrickt.reis@gmail.com>
2020-08-24 19:46:26 -0300

    Front end da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api';
+import Header from './components/Header'
+
+import './App.css'
+
+function App () {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`])
+
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Patrick Thompson Reis"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <Header title="Projetos" />
+
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  );
+};
+
+export default App;
